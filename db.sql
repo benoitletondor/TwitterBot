e66cef1c20d65831555bb517997858c6a7f5d487
Benoit Letondor <benoit.letondor@gmail.com>
2019-02-28 13:59:27 +0100

    Update db.sql with utf8mb4 as default charset

--- db.sql
+++ db.sql
@@ -1,24 +1,32 @@
--- MySQL dump 10.13  Distrib 5.5.41, for debian-linux-gnu (x86_64)
---
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `follow`
---
 
-DROP TABLE IF EXISTS `follow`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+# Dump of table favorite
+# ------------------------------------------------------------
+
+CREATE TABLE `favorite` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` bigint(20) NOT NULL,
+  `userName` varchar(100) NOT NULL,
+  `tweetId` bigint(20) NOT NULL,
+  `status` text NOT NULL,
+  `favDate` datetime NOT NULL,
+  `unfavDate` datetime DEFAULT NULL,
+  `lastAction` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+# Dump of table follow
+# ------------------------------------------------------------
+
 CREATE TABLE `follow` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `userId` bigint(20) NOT NULL,
@@ -29,71 +37,44 @@ CREATE TABLE `follow` (
   `lastAction` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `UserId` (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Table structure for table `tweet`
---
 
-DROP TABLE IF EXISTS `tweet`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
+# Dump of table reply
+# ------------------------------------------------------------
+
+CREATE TABLE `reply` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` bigint(20) NOT NULL,
+  `userName` varchar(100) NOT NULL,
+  `tweetId` bigint(20) NOT NULL,
+  `status` text NOT NULL,
+  `answer` text NOT NULL,
+  `replyDate` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `TweetId` (`tweetId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+# Dump of table tweet
+# ------------------------------------------------------------
+
 CREATE TABLE `tweet` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `content` text NOT NULL,
   `date` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `Content` (`content`(255))
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Table structure for table `reply`
---
 
-DROP TABLE IF EXISTS `reply`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `reply` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `userId` BIGINT NOT NULL,
-  `userName` VARCHAR(100) NOT NULL,
-  `tweetId` BIGINT NOT NULL,
-  `status` TEXT NOT NULL,
-  `answer` TEXT NOT NULL,
-  `replyDate` DATETIME NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `TweetId` (`tweetId`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `favorite`
---
 
-DROP TABLE IF EXISTS `favorite`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `favorite` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `userId` BIGINT NOT NULL,
-  `userName` VARCHAR(100) NOT NULL,
-  `tweetId` BIGINT NOT NULL,
-  `status` TEXT NOT NULL,
-  `favDate` DATETIME NOT NULL,
-  `unfavDate` DATETIME NULL,
-  `lastAction` DATETIME NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
